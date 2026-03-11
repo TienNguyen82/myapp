d35b5b1e2eae3d6cbfcbbf7dd4dd7298a3cdda1a
TienNguyen82 <nguyenmanhtien19822@gmail.com>
2026-03-11 12:39:02 +0700

    sw.js

--- sw.js
+++ sw.js
@@ -1,79 +1,41 @@
-const CACHE_NAME = "sisyphus-app-v1";
-
-const urlsToCache = [
-  "/",
-  "/index.html"
+const CACHE_NAME = "sisyphus-webapp-v1";
+const ASSETS = [
+  "./",
+  "./index.html",
+  "./manifest.json",
+  "./icon-192.png",
+  "./icon-512.png"
 ];
 
-// install
-
 self.addEventListener("install", event => {
-
   event.waitUntil(
-    caches.open(CACHE_NAME)
-    .then(cache => {
-      return cache.addAll(urlsToCache);
-    })
+    caches.open(CACHE_NAME).then(cache => cache.addAll(ASSETS))
   );
-
   self.skipWaiting();
-
 });
-
-// activate
 
 self.addEventListener("activate", event => {
-
   event.waitUntil(
-    caches.keys().then(keys => {
-      return Promise.all(
-        keys.filter(key => key !== CACHE_NAME)
-        .map(key => caches.delete(key))
-      );
-    })
+    caches.keys().then(keys =>
+      Promise.all(
+        keys
+          .filter(key => key !== CACHE_NAME)
+          .map(key => caches.delete(key))
+      )
+    )
   );
-
   self.clients.claim();
-
 });
 
-// fetch
-
 self.addEventListener("fetch", event => {
-
   const url = new URL(event.request.url);
 
-  // không cache API của server
-
-  if (url.hostname === "192.168.4.1") {
-    return;
-  }
+  // Không cache API Pi
+  if (url.hostname === "192.168.4.1") return;
 
   event.respondWith(
-
-    caches.match(event.request)
-    .then(response => {
-
-      if (response) {
-        return response;
-      }
-
-      return fetch(event.request)
-      .then(networkResponse => {
-
-        return caches.open(CACHE_NAME)
-        .then(cache => {
-
-          cache.put(event.request, networkResponse.clone());
-
-          return networkResponse;
-
-        });
-
-      });
-
+    caches.match(event.request).then(cached => {
+      return cached || fetch(event.request);
     })
-
   );
-
 });
